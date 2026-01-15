bd0de47e3dd68bd35d68303f5103d6b6f488c05f
llpw-build <llpw@hotmail.co.uk>
2026-01-15 12:16:12 +0000

    Add shuffle function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 const gameArea = document.getElementById("game-area");
-const cards = ["tree", "sun", "moon", "happy", "three"];
+const cards = ["tree", "sun", "moon", "happy", "three", "cat", "dog"];
 const deck = [...cards, ...cards];
+
+function shuffle(array) {
+    for (let i = array.length -1; i > 0; i-- ) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    }
+    return array;
+}
+
+shuffle(deck);
 
 for (let index = 0; index < deck.length; index++) {
 const button = document.createElement("button");
@@ -8,5 +17,6 @@ const button = document.createElement("button");
     button.textContent= deck[index];
     gameArea.append(button);
 }
+
 
 
