64e51a6bcbfb3374dbea00420066edd20cb4f6ac
llpw-build <llpw@hotmail.co.uk>
2026-01-15 13:19:25 +0000

    fix shuffle error and sort matching logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 const gameArea = document.getElementById("game-area");
 const cards = ["tree", "sun", "moon", "happy", "three", "cat", "dog"];
 const deck = [...cards, ...cards];
+let firstCard = null;
+let secondCard = null;
+let isBusy = false;
 
 function shuffle(array) {
     for (let i = array.length -1; i > 0; i-- ) {
     const randomIndex = Math.floor(Math.random() * (i + 1));
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
     }
     return array;
 }
@@ -14,9 +18,36 @@ shuffle(deck);
 for (let index = 0; index < deck.length; index++) {
 const button = document.createElement("button");
     button.classList.add("card");
+    button.classList.add("hidden")
     button.textContent= deck[index];
     gameArea.append(button);
+    button.onclick = function () {
+  if (isBusy) return;
+  if (button === firstCard) return;
+  if (!button.classList.contains("hidden")) return;
+  if (firstCard === null) {
+    firstCard = button;
+    button.classList.remove("hidden");
+    return;
+  }
+  if (secondCard === null) {
+    secondCard = button;
+    button.classList.remove("hidden");
+    if (firstCard.textContent === secondCard.textContent) {
+      firstCard.classList.add("matched");
+      secondCard.classList.add("matched");
+      firstCard = null;
+      secondCard = null;
+      return;
+    }
+    isBusy = true;
+    setTimeout(function () {
+      firstCard.classList.add("hidden");
+      secondCard.classList.add("hidden");
+      firstCard = null;
+      secondCard = null;
+      isBusy = false;
+    }, 700);
+  }
+};
 }
-
-
-
